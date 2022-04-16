a6d20529e539fd4c885da7a7429165bd2fa51416
Zhakhangir <carrymartes@gmail.com>
2022-04-16 19:05:40 +0600

    added env variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
-    REACT_APP_API_KEY: "AIzaSyA00m2ce3TM6krTDJ2muP1AUBlb2NeHCSg",
-    REACT_APP_DOMAIN: "zhakhangir-dtgurl.firebaseapp.com",
-    REACT_APP_STORAGE_KEY: "zhakhangir-dtgurl.appspot.com",
-    REACT_APP_DATABASE_URL: "https://zhakhangir-dtgurl.firebaseio.com",
-    REACT_APP_PROJECT_ID: "zhakhangir-dtgurl",
+    REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
+    REACT_APP_DOMAIN: process.env.REACT_APP_DOMAIN,
+    REACT_APP_STORAGE_KEY: process.env.REACT_APP_STORAGE_KEY,
+    REACT_APP_DATABASE_URL: process.env.REACT_APP_DATABASE_URL,
+    REACT_APP_PROJECT_ID: process.env.REACT_APP_PROJECT_ID,
   },
 };
